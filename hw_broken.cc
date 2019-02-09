87756c53a9b0e3d32f9a0660b7a5d75c539513fd
Liam <liammhendrickson@gmail.com>
2019-02-09 14:26:07 -0600

    Update hw_broken.cc

--- hw_broken.cc
+++ hw_broken.cc
@@ -4,22 +4,24 @@
 
 
 
-int deviation( int a[])
+double deviation(int* a, int n )
 {
-	int sum;
+	double sum=0;
+	double count=0;
 
-	for(size_t i = 0; i <= (a.size()-1); i++) //gets the sum of the data so that the mean can be calculated
+	for(size_t i = 0; i <= n-1; i++) //gets the sum of the data so that the mean can be calculated
 	{
 		sum += a[i];
+		count++;
 	} 
-	double mean = sum /= a.size(); //calculates the mean
+	double mean = sum / count; //calculates the mean
 	double stddev = 0;
-	for(size_t i = 0; i <= a.size() -1; i++) // calculates the square of differences between each value and the mean and sums them
+	for(size_t i = 0; i <= n-1; i++) // calculates the square of differences between each value and the mean and sums them
 	{
-		double stddev = (stddev + ((a[i] - mean) * (a[i] - mean))); 
+		stddev += ((a[i] - mean) * (a[i] - mean)); 
 	}
-	stddev /= a.size();
-	if( stddev = 0)
+	stddev /= count;
+	if( stddev == 0)
 		std::cout << "Sigma is zero." << std::endl;
 	return sqrt(stddev);
 }
